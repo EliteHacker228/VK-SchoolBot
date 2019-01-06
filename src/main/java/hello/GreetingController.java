ca6fa08462943565c884df3d21053d6c688b1183
EliteHacker228 <maximusreg@yandex.ru>
2019-01-06 21:06:00 +0500

    version two episode one

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,20 +1,30 @@
 package hello;
 
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Map;
 
 @Controller
 public class GreetingController {
 
-    @GetMapping("/greeting")
-    public String getGreeting(@RequestParam(required = true, name="name") String name, Map<String, Object> model){
-        model.put("name", name);
-
-        return "greeting";
+    @GetMapping(value = "/greeting", produces = "application/json")
+    public @ResponseBody Object getGreeting(@RequestParam(required = true, name="name") String name, Map<String, Object> model){
+//        model.put("name", name);
+//        Object[] models = {model};
+//
+//
+//        return models;
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        Gson json = gsonBuilder.create();
+        ServerNode s = new ServerNode(name);
+        return json.toJson(s);
     }
 
 
---
+++ src/main/java/hello/ServerNode.java
@@ -0,0 +1,11 @@
+package hello;
+
+public class ServerNode {
+    String node;
+
+    ServerNode(String node){
+        this.node=node;
+    }
+
+
+}
