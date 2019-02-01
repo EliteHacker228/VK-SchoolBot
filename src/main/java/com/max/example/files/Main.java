6b458d393b557a5ca30d0571a665e512505fb3a0
EliteHacker228 <mchappycraft@yandex.ru>
2019-02-01 20:38:51 +0500

    test of registration

--- src/main/java/com/max/example/files/Main.java
+++ src/main/java/com/max/example/files/Main.java
@@ -1,11 +1,18 @@
 package com.max.example.files;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
 
 @SpringBootApplication(scanBasePackages = {"com.max.example.files.datanodes", "com.max.example.files"})
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
+
 }
---
+++ src/main/java/com/max/example/files/services/LogService.java
@@ -0,0 +1,29 @@
+package com.max.example.files.services;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LogService implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        main(args);
+    }
+
+    public static void main(String[] args) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true){
+                    try {
+                        System.out.println("THREAD");
+                        Thread.sleep(1500000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+}
