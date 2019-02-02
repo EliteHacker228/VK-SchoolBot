49a98f47fdea374cca62102f0d2f433cceb6b0e0
EliteHacker228 <mchappycraft@yandex.ru>
2019-02-02 16:43:49 +0500

    refractored

--- src/main/java/com/max/example/files/services/MessageService.java
+++ src/main/java/com/max/example/files/services/MessageService.java
@@ -1,5 +1,13 @@
 package com.max.example.files.services;
 
+import com.max.example.files.datanodes.classes.Region;
+import com.max.example.files.datanodes.classes.SClass;
+import com.max.example.files.datanodes.classes.School;
+import com.max.example.files.datanodes.classes.Student;
+import com.max.example.files.datanodes.repositories.ClassesRepository;
+import com.max.example.files.datanodes.repositories.RegionsRepository;
+import com.max.example.files.datanodes.repositories.SchoolsRepository;
+import com.max.example.files.datanodes.repositories.StudentsRepository;
 import com.max.example.files.entities.VKGroupMessage;
 import com.max.example.files.entities.VKRequest;
 import com.vk.api.sdk.client.TransportClient;
@@ -8,6 +16,10 @@ import com.vk.api.sdk.client.actors.GroupActor;
 import com.vk.api.sdk.exceptions.ApiException;
 import com.vk.api.sdk.exceptions.ClientException;
 import com.vk.api.sdk.httpclient.HttpTransportClient;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 
 public class MessageService {
@@ -16,6 +28,14 @@ public class MessageService {
     private VKRequest vkRequest;
     private VKGroupMessage vkGroupMessage;
     private GroupActor actor;
+    @Autowired
+    private RegionsRepository regionsRepository;
+    @Autowired
+    private ClassesRepository classesRepository;
+    @Autowired
+    private SchoolsRepository schoolsRepository;
+    @Autowired
+    private StudentsRepository studentsRepository;
 
     public MessageService(VKRequest vkRequest){
         this.vkRequest=vkRequest;
@@ -30,6 +50,155 @@ public class MessageService {
 
     private void main(String[] args){
 
+    }
+
+    public void workMethod(){
+        if(studentsRepository.findByVkId(vkGroupMessage.getFrom_id()).isEmpty()){
+            //System.out.println("empty");
+
+            Student student = new Student();
+            student.setVkId(vkGroupMessage.getFrom_id());
+            studentsRepository.save(student);
+
+            sendMessage("Привет! Ты пока не зарегистрирован в нашей системе!" +
+                    "Для того, чтобы зарегистрироваться, ответь на несколько вопросов.");
+
+            sendMessage("Какой твой регион?");
+            int counter=1;
+
+            String regions = "";
+            for( Region region: regionsRepository.findAll()){
+                regions+=String.format("%d. "+region.getName(), counter)+"\n";
+                counter++;
+            }
+            sendMessage(regions);
+
+            return;
+        }
+
+        if(!studentsRepository.findByVkId(vkGroupMessage.getFrom_id()).isEmpty() &&
+                studentsRepository.findByVkId(vkGroupMessage.getFrom_id()).get(0).getRegionId()==null){
+            Student student = studentsRepository.findByVkId(vkGroupMessage.getFrom_id()).get(0);
+
+            if(student.getRegionId()==null){
+                if(vkGroupMessage.getText().matches("[-+]?\\d+")
+                        && Integer.parseInt(vkGroupMessage.getText())<=
+                        makeCollection(regionsRepository.findAll()).size()){
+                    student.setRegionId(Integer.parseInt(vkGroupMessage.getText()));
+                    studentsRepository.save(student);
+                    sendMessage("Регион записан!");
+                    System.out.println();
+                }else{
+                    sendMessage("Ошибка! Такого региона нет.");
+                    System.out.println();
+                    return;
+                }
+
+            }
+
+            sendMessage("Из какой ты школы?");
+            System.out.println();
+
+            String schools = "";
+            int counter=1;
+            for( School school: schoolsRepository.findAll()){
+                schools+=String.format("%d. "+school.getName(), counter)+"\n";
+                counter++;
+            }
+            sendMessage(schools);
+
+
+            return;
+
+        }
+
+        if(!studentsRepository.findByVkId(vkGroupMessage.getFrom_id()).isEmpty() &&
+                studentsRepository.findByVkId(vkGroupMessage.getFrom_id()).get(0).getSchoolId()==null){
+            Student student = studentsRepository.findByVkId(vkGroupMessage.getFrom_id()).get(0);
+
+            if(student.getSchoolId()==null){
+                if(vkGroupMessage.getText().matches("[-+]?\\d+")
+                        && Integer.parseInt(vkGroupMessage.getText())<=
+                        makeCollection(regionsRepository.findAll()).size()){
+                    student.setSchoolId(Integer.parseInt(vkGroupMessage.getText()));
+                    studentsRepository.save(student);
+                    sendMessage("Школа записана!");
+                }else{
+                    sendMessage("Ошибка! Такой школы нет.");
+                    return;
+                }
+
+            }
+
+            sendMessage("Введи свой класс (например, 7Б, 10А и т.д)");
+
+            return;
+
+        }
+
+        if(!studentsRepository.findByVkId(vkGroupMessage.getFrom_id()).isEmpty() &&
+                studentsRepository.findByVkId(vkGroupMessage.getFrom_id()).get(0).getClassId()==null){
+            Student student = studentsRepository.findByVkId(vkGroupMessage.getFrom_id()).get(0);
+            System.out.println("Ввод класса");
+
+            if(student.getClassId()==null){
+                System.out.println("Class id: "+student.getClassId());
+                String[] classNode;
+                try {
+                    classNode = stringToClass(vkGroupMessage.getText());
+                    System.out.println("Message: "+vkGroupMessage.getText());
+
+
+                    for(SClass sClass: classesRepository.findBySchoolId(student.getSchoolId())){
+                        System.out.println(sClass);
+                        if(     sClass.getNumber()==Integer.parseInt(classNode[0])
+                                && sClass.getLetter().equals(classNode[1])
+                                && sClass.getSchoolId().equals(student.getSchoolId())
+                                ){
+
+                            student.setClassId(sClass.getId());
+                            studentsRepository.save(student);
+                            sendMessage("Готово! ");
+                        }
+                    }
+
+                } catch (Exception e) {
+                    sendMessage("Такого класса нет.");
+                    return;
+                }
+            }
+
+            sendMessage("Здравствуйте!");
+
+            return;
+
+        }
+
+        sendMessage("Здравствуйте!");
+    }
+
+    public <T> Collection<T> makeCollection(Iterable<T> iter) {
+        Collection<T> list = new ArrayList<T>();
+        for (T item : iter) {
+            list.add(item);
+        }
+        return list;
+    }
+
+    public String[] stringToClass(String str) throws Exception {
+
+        str=str.toUpperCase();
+        if(str.matches("[-+]?\\d+")){
+            throw new Exception("Wrong class");
+        }
+        if(str.length()>=2 && str.length()<=3) {
+            String[] result = {str.substring(0, str.length() - 1),
+                    str.substring(str.length() - 1, str.length())
+            };
+            return result;
+        }else{
+            throw new Exception("Wrong class");
+        }
     }
 
     public void sendMessage(String text){
