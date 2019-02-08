f65266af54adf58c51ce5e3fca18a0db0b39e381
EliteHacker228 <mchappycraft@yandex.ru>
2019-02-08 15:28:35 +0500

    added attention service

--- src/main/java/com/max/example/files/services/AttentionService.java
+++ src/main/java/com/max/example/files/services/AttentionService.java
@@ -36,107 +36,111 @@ public class AttentionService {
     }
 
     public void workMethod(){
-        String text=message;
-        System.out.println("MSG: "+message);
-        if(text.equals("0")){
-            sendMessage("Отправка отменена", vkRequest.getObject().getFrom_id());
-            return;
-        }
-        if(text.split("[()]")[1].contains("*")){
-
-            int schoolId = studentsRepository.findByVkId(vkRequest.getObject().getFrom_id()).get(0).getSchoolId();
-            for(SClass sClass: classesRepository.findBySchoolId(schoolId)){
-                for(Student s: studentsRepository.findByClassId(sClass.getId())){
-                    sendMessage(text.split("[()]")[0], s.getVkId());
-                }
+        try {
+            String text = message;
+            System.out.println("MSG: " + message);
+            if (text.equals("0")) {
+                sendMessage("Отправка отменена", vkRequest.getObject().getFrom_id());
+                return;
             }
-        }else if(text.split("[()]")[1].contains("-")){
-            String[] splittedText = text.split("[()]");
-            String neededText=splittedText[1];
-            String[] sndt = neededText.split("-");
-            int val1 = Integer.parseInt(sndt[0]);
-            int val2 = Integer.parseInt(sndt[1]);
+            if (text.split("[()]")[1].contains("*")) {
+
+                int schoolId = studentsRepository.findByVkId(vkRequest.getObject().getFrom_id()).get(0).getSchoolId();
+                for (SClass sClass : classesRepository.findBySchoolId(schoolId)) {
+                    for (Student s : studentsRepository.findByClassId(sClass.getId())) {
+                        sendMessage(text.split("[()]")[0], s.getVkId());
+                    }
+                }
+            } else if (text.split("[()]")[1].contains("-")) {
+                String[] splittedText = text.split("[()]");
+                String neededText = splittedText[1];
+                String[] sndt = neededText.split("-");
+                int val1 = Integer.parseInt(sndt[0]);
+                int val2 = Integer.parseInt(sndt[1]);
 //            System.out.println("Отправим сообщения всем параллелям с "+
 //                    Math.min(val1, val2)+" по "+Math.max(val1, val2)+" класс");
-            int schoolId = studentsRepository.findByVkId(vkRequest.getObject().getFrom_id()).get(0).getSchoolId();
+                int schoolId = studentsRepository.findByVkId(vkRequest.getObject().getFrom_id()).get(0).getSchoolId();
 
-            for(SClass sClass: classesRepository.findBySchoolId(schoolId)){
-                for(int i = Math.min(val1, val2); i<=Math.max(val1, val2); i++){
-                    if(sClass.getNumber()==i){
-                        for(Student s: studentsRepository.findByClassId(sClass.getId())){
+                for (SClass sClass : classesRepository.findBySchoolId(schoolId)) {
+                    for (int i = Math.min(val1, val2); i <= Math.max(val1, val2); i++) {
+                        if (sClass.getNumber() == i) {
+                            for (Student s : studentsRepository.findByClassId(sClass.getId())) {
+                                sendMessage(text.split("[()]")[0], s.getVkId());
+                            }
+                        }
+                    }
+                }
+
+            } else if (text.split("[()]")[1].contains("!")) {
+                String[] splittedText = text.split("[()]");
+                String neededText = splittedText[1];
+                String sndt = neededText.split("!")[0];
+                Integer number = Integer.parseInt(sndt);
+                int schoolId = studentsRepository.findByVkId(vkRequest.getObject().getFrom_id()).get(0).getSchoolId();
+                for (SClass sClass : classesRepository.findBySchoolId(schoolId)) {
+                    for (Student s : studentsRepository.findByClassId(sClass.getId())) {
+                        if (classesRepository.findByid(s.getClassId()).get(0).getNumber() == number) {
                             sendMessage(text.split("[()]")[0], s.getVkId());
                         }
                     }
                 }
-            }
 
-        }else if(text.split("[()]")[1].contains("!")){
-            String[] splittedText = text.split("[()]");
-            String neededText=splittedText[1];
-            String sndt=neededText.split("!")[0];
-            Integer number = Integer.parseInt(sndt);
-            int schoolId = studentsRepository.findByVkId(vkRequest.getObject().getFrom_id()).get(0).getSchoolId();
-            for(SClass sClass: classesRepository.findBySchoolId(schoolId)){
-                for(Student s: studentsRepository.findByClassId(sClass.getId())){
-                    if(classesRepository.findByid(s.getClassId()).get(0).getNumber()==number){
-                        sendMessage(text.split("[()]")[0], s.getVkId());
+            } else if (text.split("[()]")[1].contains(",")) {
+                String[] splittedText = text.split("[()]");
+                String neededText = splittedText[1];
+                String[] sndt = neededText.split(",");
+
+                int schoolId = studentsRepository.findByVkId(vkRequest.getObject().getFrom_id()).get(0).getSchoolId();
+                //System.out.print("Отправим объявление: ");
+
+                ArrayList<SClass> classes = new ArrayList<>();
+                ArrayList<SClass> sClassArrayList = new ArrayList<SClass>(classesRepository.findBySchoolId(schoolId));
+
+                for (String s : sndt) {
+                    String letter = s.substring(s.length() - 1, s.length());//Буква
+                    String number = s.substring(0, s.length() - 1);//Число
+
+                    for (SClass sClass1 : sClassArrayList) {
+                        if (sClass1.getLetter().equals(letter) &&
+                                sClass1.getNumber() == Integer.parseInt(number)) {
+                            classes.add(sClass1);
+                        }
+
                     }
                 }
-            }
 
-        }else if(text.split("[()]")[1].contains(",")){
-            String[] splittedText = text.split("[()]");
-            String neededText=splittedText[1];
-            String[] sndt=neededText.split(",");
-
-            int schoolId = studentsRepository.findByVkId(vkRequest.getObject().getFrom_id()).get(0).getSchoolId();
-            //System.out.print("Отправим объявление: ");
-
-            ArrayList<SClass> classes = new ArrayList<>();
-            ArrayList<SClass> sClassArrayList = new ArrayList<SClass>(classesRepository.findBySchoolId(schoolId));
-
-            for(String s: sndt){
-                String letter = s.substring(s.length()-1,s.length());//Буква
-                String number = s.substring(0,s.length()-1);//Число
-
-                for(SClass sClass1: sClassArrayList) {
-                    if(sClass1.getLetter().equals(letter) &&
-                       sClass1.getNumber() == Integer.parseInt(number)){
-                        classes.add(sClass1);
-                    }
-
-                }
-            }
-
-            for(SClass sClass: sClassArrayList){
-                ArrayList<Student> students = new ArrayList<>(studentsRepository.findByClassId(sClass.getId()));
-                for(Student student: students){
-                    sendMessage(text.split("[()]")[0], student.getVkId());
-                }
-            }
-            //System.out.println(" классам");
-
-        }else if(messageOneClassValidator(text)){
-            String[] splittedText = text.split("[()]");
-            String s=splittedText[1];
-
-            int schoolId = studentsRepository.findByVkId(vkRequest.getObject().getFrom_id()).get(0).getSchoolId();
-            String letter = s.substring(s.length()-1,s.length());//Буква
-            String number = s.substring(0,s.length()-1);//Число
-
-            ArrayList<SClass> sClassArrayList = new ArrayList<SClass>(classesRepository.findBySchoolId(schoolId));
-            for(SClass sClass: sClassArrayList){
-                if(sClass.getLetter().equals(letter) && sClass.getNumber()==Integer.parseInt(number)){
+                for (SClass sClass : sClassArrayList) {
                     ArrayList<Student> students = new ArrayList<>(studentsRepository.findByClassId(sClass.getId()));
-                    for(Student student: students){
-                        sendMessage(splittedText[0], student.getVkId());
+                    for (Student student : students) {
+                        sendMessage(text.split("[()]")[0], student.getVkId());
                     }
                 }
-            }
+                //System.out.println(" классам");
 
-        }else{
+            } else if (messageOneClassValidator(text)) {
+                String[] splittedText = text.split("[()]");
+                String s = splittedText[1];
+
+                int schoolId = studentsRepository.findByVkId(vkRequest.getObject().getFrom_id()).get(0).getSchoolId();
+                String letter = s.substring(s.length() - 1, s.length());//Буква
+                String number = s.substring(0, s.length() - 1);//Число
+
+                ArrayList<SClass> sClassArrayList = new ArrayList<SClass>(classesRepository.findBySchoolId(schoolId));
+                for (SClass sClass : sClassArrayList) {
+                    if (sClass.getLetter().equals(letter) && sClass.getNumber() == Integer.parseInt(number)) {
+                        ArrayList<Student> students = new ArrayList<>(studentsRepository.findByClassId(sClass.getId()));
+                        for (Student student : students) {
+                            sendMessage(splittedText[0], student.getVkId());
+                        }
+                    }
+                }
+
+            } else {
+                sendMessage("Неверно указан класс/параллель", vkRequest.getObject().getFrom_id());
+                //sendMessage("Отправлено", vkRequest.getObject().getFrom_id());
+            }
+        }catch (Exception e){
             sendMessage("Неверно указан класс/параллель", vkRequest.getObject().getFrom_id());
-            //sendMessage("Отправлено", vkRequest.getObject().getFrom_id());
         }
     }
 
