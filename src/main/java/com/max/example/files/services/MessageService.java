ecfcdcafbb7c0d39b3d028ab331412d0d58e2796
EliteHacker228 <mchappycraft@yandex.ru>
2019-02-20 22:10:21 +0500

    school reg system + fields validator x

--- src/main/java/com/max/example/files/services/MessageService.java
+++ src/main/java/com/max/example/files/services/MessageService.java
@@ -116,21 +116,21 @@ public class MessageService {
 //            UserXtrCounters userXtrCounters = ugqMap.get(0);
 //            System.out.println(userXtrCounters.getFirstName());
 //            System.out.println(userXtrCounters.getLastName());
-                if(student.getRegionId()==null){
-                    student.setStatus(StudentStatus.STUDENT_REGION_REGISTRATION.name());
-                    studentsRepository.save(student);
-                    return;
-
-                }else if(student.getSchoolId()==null){
-                    student.setStatus(StudentStatus.STUDENT_SCHOOL_REGISTRATION.name());
-                    studentsRepository.save(student);
-                    return;
-
-                }else if(student.getClassId()==null){
-                    student.setStatus(StudentStatus.STUDENT_CLASS_REGISTRATION.name());
-                    studentsRepository.save(student);
-                    return;
-                }
+//                if(student.getRegionId()==null){
+//                    student.setStatus(StudentStatus.STUDENT_REGION_REGISTRATION.name());
+//                    studentsRepository.save(student);
+//                    return;
+//
+//                }else if(student.getSchoolId()==null){
+//                    student.setStatus(StudentStatus.STUDENT_SCHOOL_REGISTRATION.name());
+//                    studentsRepository.save(student);
+//                    return;
+//
+//                }else if(student.getClassId()==null){
+//                    student.setStatus(StudentStatus.STUDENT_CLASS_REGISTRATION.name());
+//                    studentsRepository.save(student);
+//                    return;
+//                }
 
             switch (StudentStatus.valueOf(student.getStatus())) {
                 case STUDENT_REGION_REGISTRATION:
