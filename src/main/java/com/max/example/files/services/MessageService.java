5b0e2b80531770be8e7c273f64875fbf5b1cb6a7
EliteHacker228 <mchappycraft@yandex.ru>
2019-02-24 21:41:37 +0500

    Schedule node getting system

--- src/main/java/com/max/example/files/services/MessageService.java
+++ src/main/java/com/max/example/files/services/MessageService.java
@@ -112,44 +112,48 @@ public class MessageService {
 //            UserXtrCounters userXtrCounters = ugqMap.get(0);
 //            System.out.println(userXtrCounters.getFirstName());
 //            System.out.println(userXtrCounters.getLastName());
-            if(student.getRegionId()==null
-                    && !student.getStatus().equals(StudentStatus.STUDENT_REGION_REGISTRATION.name())){
 
-                int counter = 1;
-                String msg = "Регион не зарегистрирован! Укажите ваш регион: \n";
-                String regions = "";
-                for (Region region : regionsRepository.findAll()) {
-                    regions += String.format("%d. " + region.getName(), counter) + "\n";
-                    counter++;
-                }
-                sendMessage(msg+regions);
-                student.setStatus(StudentStatus.STUDENT_REGION_REGISTRATION.name());
-                studentsRepository.save(student);
-                return;
-            }else if(student.getSchoolId()==null
-                    && !student.getStatus().equals(StudentStatus.STUDENT_SCHOOL_REGISTRATION.name())){
-
-                sendMessage("Укажи номер своей школы. \n Если не видишь в списке свою школу - отправь её официальное название" +
-                        "(например, МАОУ СОШ №67 с УИОП), и она зарегистрируется в системе.");
-                String schools = "";
-                int counter = 1;
-                for (School school : schoolsRepository.findAll()) {
-                    schools += String.format("%d. " + school.getName(), counter) + "\n";
-                    counter++;
-                }
-                sendMessage(schools);
-
-                student.setStatus(StudentStatus.STUDENT_SCHOOL_REGISTRATION.name());
-                studentsRepository.save(student);
-                return;
-            }else if(student.getClassId()==null
-                    && !student.getStatus().equals(StudentStatus.STUDENT_CLASS_REGISTRATION.name())){
-
-                sendMessage("Введи свой класс (например, 7Б, 10А и т.д)");
-                student.setStatus(StudentStatus.STUDENT_CLASS_REGISTRATION.name());
-                studentsRepository.save(student);
-                return;
-            }
+            /**
+             * TODO: Fix this
+             */
+//            if(student.getRegionId()==null
+//                    && !student.getStatus().equals(StudentStatus.STUDENT_REGION_REGISTRATION.name())){
+//
+//                int counter = 1;
+//                String msg = "Регион не зарегистрирован! Укажите ваш регион: \n";
+//                String regions = "";
+//                for (Region region : regionsRepository.findAll()) {
+//                    regions += String.format("%d. " + region.getName(), counter) + "\n";
+//                    counter++;
+//                }
+//                sendMessage(msg+regions);
+//                student.setStatus(StudentStatus.STUDENT_REGION_REGISTRATION.name());
+//                studentsRepository.save(student);
+//                return;
+//            }else if(student.getSchoolId()==null
+//                    && !student.getStatus().equals(StudentStatus.STUDENT_SCHOOL_REGISTRATION.name())){
+//
+//                sendMessage("Укажи номер своей школы. \n Если не видишь в списке свою школу - отправь её официальное название" +
+//                        "(например, МАОУ СОШ №67 с УИОП), и она зарегистрируется в системе.");
+//                String schools = "";
+//                int counter = 1;
+//                for (School school : schoolsRepository.findAll()) {
+//                    schools += String.format("%d. " + school.getName(), counter) + "\n";
+//                    counter++;
+//                }
+//                sendMessage(schools);
+//
+//                student.setStatus(StudentStatus.STUDENT_SCHOOL_REGISTRATION.name());
+//                studentsRepository.save(student);
+//                return;
+//            }else if(student.getClassId()==null
+//                    && !student.getStatus().equals(StudentStatus.STUDENT_CLASS_REGISTRATION.name())){
+//
+//                sendMessage("Введи свой класс (например, 7Б, 10А и т.д)");
+//                student.setStatus(StudentStatus.STUDENT_CLASS_REGISTRATION.name());
+//                studentsRepository.save(student);
+//                return;
+//            }
 
 //            else{
 //                student.setStatus(StudentStatus.STUDENT_IN_ACTION.name());
