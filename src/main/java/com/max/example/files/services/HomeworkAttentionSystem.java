eb376c7b7edef8aa11921e863139221a7181e724
EliteHacker228 <mchappycraft@yandex.ru>
2019-04-05 20:58:04 +0500

    Reworked homework removing function

--- src/main/java/com/max/example/files/services/HomeworkAttentionSystem.java
+++ src/main/java/com/max/example/files/services/HomeworkAttentionSystem.java
@@ -62,7 +62,7 @@ public class HomeworkAttentionSystem implements CommandLineRunner {
                             } catch (ClientException e) {
                                 e.printStackTrace();
                             }
-                        } else if (hw.getRemindDate() >= date.getTime() && hw.getReminded()) {
+                        } else if (hw.getRemindDate() + 43200000L <= date.getTime()) {
                             homeworkRepository.deleteById(hw.getId());
                         }
                     }
